ac9d207cbe5bb38e6e5bcf54563a4d65714b35f5
Franco Orlandi <69011191+xFraZk@users.noreply.github.com>
2025-11-30 17:31:21 -0300

    Update modal-sonido.js

--- modal-sonido.js
+++ modal-sonido.js
@@ -1,13 +1,1 @@
- const gameModal = document.getElementById('gameModal');
-        const fsIframe = document.getElementById('fullscreen-iframe');
-        const gameUrl = "p5/index.html"; 
 
-
-        gameModal.addEventListener('show.bs.modal', function () {
-            fsIframe.src = gameUrl;
-        });
-
-
-        gameModal.addEventListener('hidden.bs.modal', function () {
-            fsIframe.src = "";
-        });
